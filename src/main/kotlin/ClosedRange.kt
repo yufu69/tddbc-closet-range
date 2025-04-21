e25eb508c07432e50a20d8f8114ea2308b41cacd
yufu <atashism@gmail.com>
2025-04-22 08:16:30 +0900

    IntelliJ のうそつき

--- src/main/kotlin/ClosedRange.kt
+++ src/main/kotlin/ClosedRange.kt
@@ -1,6 +1,10 @@
 package org.example
 
 data class ClosedRange(val lowerEndpoint: Int, val upperEndpoint: Int) {
+    init {
+        require(lowerEndpoint <= upperEndpoint)
+    }
+
     override fun toString(): String {
         return "[${lowerEndpoint},${upperEndpoint}]"
     }
